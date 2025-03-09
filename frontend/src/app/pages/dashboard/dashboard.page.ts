5ee22da068adea32e16d0ba1caba69e841c9c368
poppy <poppypopcorn@gmail.com>
2025-03-09 12:30:52 +0700

    header

--- frontend/src/app/pages/dashboard/dashboard.page.ts
+++ frontend/src/app/pages/dashboard/dashboard.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true, 
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, HeaderComponent],
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.css'] 
 })
